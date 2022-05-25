fb4a6941ad1dcea38740e2c4f73bb52a51637762
Denis Levdanskiy <d.levdanskiy@gmail.com>
2022-05-25 11:59:50 +0400

    add task_1 course qa

---
+++ task_1/homework/script/task_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir homework
+
+cd homework
+
+mkdir folder_1 folder_2 folder_3
+
+cd folder_3
+
+touch file_1.txt file_2.txt file_3.txt file_4.json file_5.json
+
+mkdir folder_4 folder_5 folder_6
+
+ls -la
+
+mv file_1.txt file_5.json ../folder_2
